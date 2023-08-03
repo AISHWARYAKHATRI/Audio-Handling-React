f3d4a20bf395d4d9dc664ca5238521c4b88898eb
AISHWARYAKHATRI <191200107021@asoit.edu.in>
2023-08-03 08:38:07 +0530

    custom audio player

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRef, useState } from "react";
-
+import audio from "./audio.mp3";
 // Helper function to convert Base64 to Blob
 function base64ToBlob(base64Data) {
   const byteCharacters = atob(base64Data.split(",")[1]);
@@ -55,11 +55,71 @@ function App() {
     }
   };
 
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const audioRef = useRef();
+
+  const handlePlayPause = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleTimeUpdate = () => {
+    const currentTime = audioRef.current.currentTime;
+    const duration = audioRef.current.duration;
+    setProgress((currentTime / duration) * 100);
+  };
+
+  const handleSeek = (e) => {
+    const seekTime = (e.target.value * audioRef.current.duration) / 100;
+    audioRef.current.currentTime = seekTime;
+    setProgress(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <input type="file" onChange={(e) => handleAudioFile(e)} />
-      <audio controls src="" ref={inputRef}></audio>
-    </div>
+    <>
+      <div className="App">
+        <input type="file" onChange={(e) => handleAudioFile(e)} />
+        <audio controls src="" ref={inputRef}></audio>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div style={{ width: "70%" }}>
+            <audio
+              ref={audioRef}
+              src={audio}
+              onTimeUpdate={handleTimeUpdate}
+              onEnded={() => setIsPlaying(false)}
+            />
+            <div style={{ width: "100%", height: "20px", background: "#ccc" }}>
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value={progress}
+                onChange={handleSeek}
+                style={{ width: "100%", height: "100%" }}
+              />
+            </div>
+          </div>
+          <button onClick={handlePlayPause} style={{ marginLeft: "10px" }}>
+            {isPlaying ? (
+              <i class="fa-solid fa-pause" style={{ color: "#ffffff" }}></i>
+            ) : (
+              <i class="fa-thin fa-play" style={{ color: "#ffffff" }}></i>
+            )}
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
